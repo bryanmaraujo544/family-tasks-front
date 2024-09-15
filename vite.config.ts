4db8e4c382a1be5321f5366070fbaffee44bb460
Bryan Martins <bryan.araujo@curseduca.com>
2024-09-15 20:44:45 -0300

    chore: react testing library setup

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,10 @@ export default defineConfig({
             '@': path.resolve(__dirname, './src'),
         },
     },
+    test: {
+        environment: 'jsdom',
+        setupFiles: ['./tests/setup.ts'],
+        testMatch: ['./tests/**/*.test.tsx'],
+        globals: true,
+    },
 });
